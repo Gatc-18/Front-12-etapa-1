59c6b252f4e24c0babc641f071b6a244ec4e05c9
Jose Garcia <jose.garcia@agileinnova.org>
2022-05-27 17:07:55 -0500

    sesion2

---
+++ sesion_2/scripts/blucles.js
@@ -0,0 +1,13 @@
+
+// let x = 0
+
+// for( i = 0; i <= 10; i++ ){
+//     console.log("la variable i: " + i);
+// }
+
+let palabra = prompt("Ingresa una palabra")
+
+
+while ( palabra != "salir" ) {
+    palabra = prompt("Ingresa una palabra")
+}
---
+++ sesion_2/scripts/condicionales.js
@@ -0,0 +1,97 @@
+// Condicionales simples
+let a = 8;
+let b = 7;
+
+// true
+// false
+
+// console.log( typeof(a) );
+
+// if ( ((a > 10) || (a === 8)) || (a === 8)){
+//     console.log("Acerto");
+// }
+
+// Condicionales dobles
+// if ( a > 10 ) {
+//     console.log("a es mayor a 10");
+// } else {
+//     console.log("a es menor a 10");
+// }
+
+
+// Condicionales multiples
+
+// if ( a == 5 ){
+//     console.log("a es igual a 5");
+// } else if ( a == 6 ) {
+//     console.log("a es igual a 6");
+// } else if ( a > 5 ) {
+//     console.log("a es mayor a 5");
+// } else {
+//     console.log("a es menor a 5");
+// }
+
+// Condicionales anidados
+
+// if ( a == 6 ){  // si
+
+//     if ( typeof(a) == "number") {
+//         console.log("El tipo de dato es Number y si es igual a 6");
+//     } else {
+//         console.log("El tipo de dato no es Number pero si es igual a 6");
+//     }
+
+// } else { // Si no
+//     console.log("no es igual a 6");
+// }
+
+// Logiaca booleana
+
+// si = true
+// no = false
+
+// true
+//false
+
+// =    ==
+
+// ! Negación
+// == Comparacion (evalua el valor)
+// === Comparacion estricta (evalua tipo y valor)
+// != (Distinto evalua el valor)
+// !== (Distinto evalua el valor y tipo)
+// && Conjucion
+// || Disyuncion
+
+// console.log(!true); // false
+// console.log(!false); // true
+
+// console.log( 1 == "1" ); 
+
+// console.log( "Jose" === "jose" );
+
+// console.log( "1" != "Jose" );
+
+// console.log( "Jose" !== "Jose" );
+
+// console.log( true && true && false );
+
+// console.log( true || false || false );
+
+// switch ( a, b ) { // 8, 7
+//     case a > b: //true
+//         console.log("a es igual a 5")
+//         break;
+//     case 6:
+//         console.log("a es igual a 6")
+//         break;
+//     case 7:
+//         console.log("a es igual a 7")
+//         break;
+//     case 8:
+//         console.log("a es igual a 8")
+//         break;
+//     default:
+//         console.log("No hay casos para ese valor")
+//         break;
+// }
