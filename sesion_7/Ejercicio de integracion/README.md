1936fb3d4d8167069216f71dd8db76368686980e
Jose Garcia <jose.garcia@agileinnova.org>
2022-06-07 12:06:21 -0500

    ejercicio

---
+++ sesion_7/Ejercicio de integracion/README.md
@@ -0,0 +1,5 @@
+# Guia de estilos
+
+Fuente = https://fonts.googleapis.com/css2?family=Fredoka&display=swap
+Background login = https://i.pinimg.com/originals/a2/fb/23/a2fb230ac13e58c9232bc22892220593.png
+Background home/details = https://images.hdqwalls.com/wallpapers/clash-royale-clan-wars-5k-7j.jpg
---
+++ sesion_7/Ejercicio de integracion/assets/data/data.js
@@ -0,0 +1,97 @@
+export const cards = [
+    {
+      id: 1,
+      img: "../images/archer-queen.webp",
+      name: "archer-queen",
+      description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    {
+      id: 2,
+      img: "../images/arrows.webp",
+      name: "arrows",
+      description: "The Arrows card is unlocked from the Training Camp (Tutorial). It is an area damage spell with a very wide radius and low-moderate damage that quickly launches three volleys in a row. An Arrows card costs 3 Elixir to deploy."
+    },
+    {
+      id: 3,
+      img: "../images/baby-dragon.webp",
+      name: "baby-dragon",
+      description: "The Baby Dragon is the 11th Troop unlocked in the standard Barracks, It is a fearsome flying unit and is capable of attacking both ground and air units with splash damage. When no allied aerial units are within 4.5 tiles from the Baby Dragon, its Tantrum ability becomes active."
+    },
+    {
+      id: 4,
+      img: "../images/balloon.webp",
+      name: "balloon"
+    },
+    {
+      id: 5,
+      img: "../images/barbarian-barrel.webp",
+      name: "barbarian-barrel",
+      description: "Balloons are promoted Wall Breakers that now attack from a hot air balloon. They drop bombs towards the ground with a large area splash damage, which can destroy a wide range of ground targets, but can be easily taken out by any anti-air buildings or air-targeting splash-damaging buildings"
+    },
+    {
+      id: 6,
+      img: "../images/barbarians.webp",
+      name: "barbarians",
+      description: "The Barbarian is a single-target melee troop. He is the first troop unlocked in the Barracks. He appears as a male kilt-clad warrior with an angry, battle-ready expression, hungry for destruction. He has close-cropped blond hair and a long, yellow horseshoe mustache."
+    },
+    {
+      id: 7,
+      img: "../images/clone.webp",
+      name: "clone",
+      description: "The Clone Spell is 6th Elixir Spell unlocked at level 5 Spell Factory that requires the Town Hall to be at level 10. As the name of this spell implies, it clones troops that enter the spell's radius. The copied troops are equal in level to the original troop, and appear with full health."
+    },
+    {
+      id: 8,
+      img: "../images/valkyrie.webp",
+      name: "valkyrie",
+      description: "The Valkyrie is a troop unlocked once the Dark Barracks is upgraded to level 3 which requires Town Hall level 8. She is a fierce warrior with orange hair, dressed in a long leather loincloth with matching leather brassiere and knee-high boots. She wields a massive double-bladed axe, which is about half of her size."
+    },
+    {
+      id: 9,
+      img: "../images/electro-spirit.webp",
+      name: "electro-spirit",
+      description: "The Electro Spirit card is unlocked from the Electro Valley (Arena 11). It is a single-target, low-ranged ground troop with low hitpoints and damage. Its physical appearance is similar to an Ice Spirit, but its body is purple and it has a bright blue mustache."
+    },
+    {
+      id: 10,
+      img: "../images/fireball.webp",
+      name: "fireball",
+      description: "The Fireball card is unlocked from the Training Camp (Tutorial). It is an area-damage spell with a medium radius and moderately high damage, and it will push back certain small troops. A Fireball card costs 4 Elixir to deploy."
+    },
+    {
+      id: 11,
+      img: "../images/graveyard.webp",
+      name: "graveyard",
+      description: "The Graveyard card is unlocked from the Spooky Town (Arena 12) or a Legendary Chest. It is a spell with a wide radius that spawns 15 Skeletons. The Skeletons appear 1 at a time on the edge of the spell's radius in a random order. The Graveyard can be cast anywhere in the Arena. A Graveyard card costs 5 Elixir to deploy"
+    },
+    {
+      id: 12,
+      img: "../images/heal-spirit.webp",
+      name: "heal-spirit",
+      description: "The Heal Spirit card is unlocked from the Rascal's Hideout (Arena 13). It is a short-ranged area damage troop with low hitpoints and damage. Its physical appearance is similar to an Ice Spirit, but it is a bright yellow color and is shaped more closely to a rectangle."
+    },
+    {
+      id: 13,
+      img: "../images/lava-hound.webp",
+      name: "lava-hound",
+      description: "What is Lava Hound? These fiery beasts can't resist chasing after Air Defenses, providing excellent protection for other troops. Once destroyed, they erupt into many smaller, weaker menaces."
+    },
+    {
+      id: 14,
+      img: "../images/lumberjack.webp",
+      name: "lumberjack",
+      description: "He is a very fast, single-target, melee troop with moderate hitpoints. Upon death, he drops a bottle of Rage that increases the attack and movement speed of friendly troops and buildings in the spell's effective radius. A Lumberjack card costs 4 Elixir to deploy."
+    },
+    {
+      id: 15,
+      img: "../images/magic-archer.webp",
+      name: "magic-archer",
+      description: "The Magic Archer card is unlocked from the Rascal's Hideout (Arena 13) or a Legendary Chest. He is an area damage, long-ranged troop with moderately low hitpoints and damage. The Magic Archer shoots arrows which go linearly to damage his target and any other units in the arrow's path. A Magic Archer card costs 4 Elixir to deploy."
+    },
+    {
+      id: 16,
+      img: "../images/miner.webp",
+      name: "miner",
+      description: "The Miner is the twelfth Elixir troop unlocked in the level 12 Barracks, which requires Town Hall level 10. The Miner is a single-target troop that burrows underground when not engaged with enemy troops or buildings. This allows him to have the useful ability of not being affected by walls and bypassing all traps"
+    }
+  ]
